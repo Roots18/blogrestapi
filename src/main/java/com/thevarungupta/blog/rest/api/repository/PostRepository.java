6fc9149cee22a38e506eca48a172e772374480cd
roots18 <rutujavtandalwade@gmail.com>
2025-03-27 02:31:23 +0530

    Initial commit

--- src/main/java/com/thevarungupta/blog/rest/api/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thevarungupta.blog.rest.api.repository;
-
-import com.thevarungupta.blog.rest.api.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
